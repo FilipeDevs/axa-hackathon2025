63cb1d49d7347ec4a79ecdd4a02a2ac23c6cccdd
Julien Pollart <jpollart@gmail.com>
2025-04-02 11:45:07 +0200

    update token endpoint and modify token retrieval logic in getToken.py

--- getToken.py
+++ getToken.py
@@ -5,14 +5,14 @@ import requests
 
 def get_token():
     url = "http://hackathon2025-prod-env.eba-sa8j9hq2.eu-central-1.elasticbeanstalk.com"
-    endpoint = "/api/security/token/5103fe9d-aac9-4027-adf8-1f9a9ffddffd"
+    endpoint = "/api/security/token/b81ddf31-c627-4f18-b67e-043aba294249"
 
     headers = {"accept": "application/json"}
     response = requests.get(url + endpoint, headers=headers)
     if response.status_code == 200:
-        token = response.json().get('hash')
-        print("Token:", token)
-        print("Token retrieved successfully.")
+        # token = response.json().get('hash')
+        token = response.json()
+        print(token)
         return
     else:
         print("Failed to retrieve token. Status code:", response.status_code)
