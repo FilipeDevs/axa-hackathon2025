d963af5dca7b03d91e6a96c763371f31eba92558
Filipe Pereira <filipe.pereira.dev@gmail.com>
2025-04-02 11:33:52 +0200

    fix team uuid

--- getToken.py
+++ getToken.py
@@ -5,7 +5,7 @@ import requests
 
 def get_token():
     url = "http://hackathon2025-prod-env.eba-sa8j9hq2.eu-central-1.elasticbeanstalk.com"
-    endpoint = "/api/security/token/c7deec0b-c680-4315-a85c-a5c30a2b2b82"
+    endpoint = "/api/security/token/b81ddf31-c627-4f18-b67e-043aba294249"
 
     headers = {"accept": "application/json"}
     response = requests.get(url + endpoint, headers=headers)
